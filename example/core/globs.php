1823c8946fe3bf58ccaed67501e2b9897915ac55
Fantablup <84356277+Fantablup@users.noreply.github.com>
2021-08-24 01:36:35 +0200

    Add files via upload

--- example/core/globs.php
+++ example/core/globs.php
@@ -4,6 +4,7 @@ The first file that must be included in the other files.
 It starts a session and load all settings, and the DB class.
 */
 		if(!isset($_SESSION))session_start(); // just nice to have if you need session variables.
+
 	define('DB_SERVER', 'localhost');
 	define('DB_PORT', '3306');
 	define('DB_SERVER_USERNAME', 'demo');
