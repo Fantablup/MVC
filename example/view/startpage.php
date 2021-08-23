1b24db1d031324cdb8cd1074c3394bf84f501b15
Fantablup <84356277+Fantablup@users.noreply.github.com>
2021-08-23 18:43:29 +0200

    Add files via upload

--- example/view/startpage.php
+++ example/view/startpage.php
@@ -2,30 +2,30 @@
 include_once 'templates/template.php';
 class startPageView
 {
-private $model;
-private $controller;
+	private $model;
+	private $controller;
 
-public function __construct($model, $tpl) {
+	public function __construct($model, $tpl) {
 		$this->model = $model;
 		$this->tpl = $tpl;
-}
+	}
 
-public function output(){
-	$content = <<<"EOT"
-		<div class="startBox defaultBox">
-			<div class="mainHeader">Start {$this->tpl->title}</div>
-			<div class="mainContext">
-			  Welcome to this demo of MVC.
-			  A simple understanding example of how to create your own MVC framework.
-			  <br><br>
-			  This demo have four views + the test view.
-			  <br><br>
-			  I created this for learning MVC.
+	public function output(){
+		$content = <<<"EOT"
+			<div class="startBox defaultBox">
+				<div class="mainHeader">Start {$this->tpl->title}</div>
+				<div class="mainContext">
+				Welcome to this demo of MVC.
+				A simple understanding example of how to create your own MVC framework.
+				<br><br>
+				This demo have four views + the test view.
+				<br><br>
+				I created this for learning MVC.
+				</div>
 			</div>
-		</div>
-		EOT;
-		return $content;
-}
+			EOT;
+			return $content;
+	}
 
 }
 ?>
